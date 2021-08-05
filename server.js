4caef60e3448555616c98d1119c2566a68e7d51d
javmarina <javmarina@uma.es>
2021-08-05 14:16:48 +0200

    Second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const http = require("http");
 const server = http.createServer(app);
